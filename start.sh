c7963bc21a953e0c5f837ba622800657921925f4
Christos Ploutarchou <cploutarchou@gmail.com>
2021-08-04 21:19:25 +0300

    correction

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker docker build -t jenkins-docker .
+docker build -t jenkins-docker .
 docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home/ -v /var/run/docker.sock:/var/run/docker.sock --name jenkins -d jenkins-docker
